ec99db4580f49e19479cd6cb787667398dc4d06e
sunnymoon1314 <moonysun1314@gmail.com>
2024-05-08 20:30:40 +0800

    Added test scripts.

---
+++ index.test.js
@@ -0,0 +1,30 @@
+const { firsthandler, secondhandler } = require('./index')
+
+describe('firsthandler', () => {
+  it('should return status code 200 and the correct message', async () => {
+    const event = {};
+    const expectedResponse = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "This is get function!",
+      }, null, 2),
+    };
+    const result = await firsthandler(event);
+    expect(result).toEqual(expectedResponse);
+  });
+});
+
+describe('secondhandler', () => {
+  it('should return status code 200 and the correct message', async () => {
+    const event = {};
+    const expectedResponse = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "This is post function!",
+      }, null, 2),
+    };
+    const result = await secondhandler(event);
+    expect(result).toEqual(expectedResponse);
+  });
+});
+  
